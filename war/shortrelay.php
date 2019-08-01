9a3b2e95f30e43591bb1f85e5add496939413973
pf <paul@falstad.com>
2019-08-01 14:00:40 -0700

    fix export as link short url creation

---
+++ war/shortrelay.php
@@ -0,0 +1,17 @@
+<?php
+// 
+// This module acts as a relay to any URL shortener with a suitable API
+// update API call as needed if using API other than tinyurl.com
+//
+//
+    $serveraddr='http' . (isset($_SERVER['HTTPS']) ? 's' : '') . '://' . "{$_SERVER['HTTP_HOST']}";
+	$s=parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+	$path= str_replace('shortrelay.php','Ripple.html',$s);
+	$ch = curl_init();
+	$v=urlencode($_GET["v"]);
+	curl_setopt($ch, CURLOPT_URL, 'http://tinyurl.com/api-create.php?url='. $serveraddr . $path . $v);
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+	echo curl_exec($ch);
+//    echo $serveraddr . $path .$v;
+	exit;
+?>
